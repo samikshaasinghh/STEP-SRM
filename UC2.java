66dd01dc5befba80be54f8916a512b812708e8ad
samikshaasinghh <samikshaasinghh983@gmail.com>
2026-03-03 11:21:05 +0530

    UC2

--- UC2.java
+++ UC2.java
@@ -1,5 +1,13 @@
 public class UC2 {
+
     public static void main(String[] args) {
-        System.out.println("Namaste");
+
+        System.out.println(" *****   *****   *****   ***** ");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println(" *****   *****   *****   ***** ");
     }
 }
